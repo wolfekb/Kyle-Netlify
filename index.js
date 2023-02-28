704bf7b0c58796b761ff11a442bf137f9859a709
Kyle Wolfe <kwolfe@proforma.com>
2023-02-28 13:09:25 -0500

    a

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
-netlifyIdentity.on('init', () => {
-    initUser = netlifyIdentity.currentUser();
+
+
+const app = angular.module('groupPage', ['ngRoute', 'ngSanitize']);
+
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: 'landing.html'
+        })
+        .when("/home", {
+            templateUrl: 'landing.html'
+        })
+        .when("/about", {
+            templateUrl: 'about.html'
+        })
+        .when("/samplePost", {
+            templateUrl: 'sample-post.html'
+        })
+        .when("/contact", {
+            templateUrl: 'contact.html'
+        });
+
 });
 
-netlifyIdentity.on('login', () => {
-    if (initUser == null) {
-        window.location.replace('landing')
-    }
-    netlifyIdentity.close();
+app.controller("groupCtrl", function ($scope) {
+    netlifyIdentity.on('init', () => {
+        initUser = netlifyIdentity.currentUser();
+    });
+    
+    netlifyIdentity.on('login', () => {
+        if (initUser == null) {
+            window.location.replace('landing')
+        }
+        netlifyIdentity.close();
+    });
+    
+    netlifyIdentity.on('logout', () => {
+        netlifyIdentity.close();
+        window.location.replace('/')
+            
+    });
 });
-
-netlifyIdentity.on('logout', () => {
-    netlifyIdentity.close();
-    window.location.replace('/')
-        
-});
-
-// const app = angular.module('groupPage', ['ngRoute', 'ngSanitize']);
-
-
-// app.config(function ($routeProvider) {
-//     $routeProvider
-//         // .when("/", {
-//         //     templateUrl: 'landing.html'
-//         // })
-//         // .when("/home", {
-//         //     templateUrl: 'landing.html'
-//         // })
-//         // .when("/about", {
-//         //     templateUrl: 'about.html'
-//         // })
-//         // .when("/samplePost", {
-//         //     templateUrl: 'sample-post.html'
-//         // })
-//         // .when("/contact", {
-//         //     templateUrl: 'contact.html'
-//         // });
-
-// });
-
-// app.controller("groupCtrl", function ($scope) {
-// });
